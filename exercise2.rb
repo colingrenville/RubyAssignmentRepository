dbc2b12e6b4e1cbad638adbc6d5943e26c4635f6
Colin Grenville <colingrenville0@gmail.com>
2015-10-27 13:48:08 -0400

    add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+meal = 55
+tip = meal * 0.2
+puts "A #{meal} meal with a good tip of #{tip} costs " + (meal + tip).to_s
+
+puts "Colin is " + 18.to_s + " years old!"
+
+number = 45628 * 7839
+puts "A cool number is #{number}!"
+
+(true && false) || (false && true) || !(false && false)
